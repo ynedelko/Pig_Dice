3243f02f073e4c0269360b08c671f86b166b3eeb
Alyssa Horrocks & Bunker <student@epicodus.com>
2015-09-16 16:49:42 -0700

    refactored code and adds README.md file

---
+++ README.md
@@ -0,0 +1,6 @@
+enter 2 player names and hit enter, enter number of dice you want to play with.
+hit roll button for respective player, when you want to hold press hold button (might be broken).
+Hold is the only way to win game, after you get to minimum points hit hold button (but you know, its broken.)
+computer AI construct is made but not working or used at all yet.
+
+good luck.
--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function Player(name, turnScore, totalScore) {
 }
 
 Player.prototype.addScore = function (die1) {
-  this.turnScore = die1.value + this.turnScore;
+  this.turnScore = die1 + this.turnScore;
   return this.turnScore;
 };
 
@@ -28,7 +28,7 @@ function Computer(name, turnScore, totalScore) {
   this.name = name;
   this.turnScore = turnScore;
   this.totalScore = totalScore;
-};
+}
 
 $(document).ready(function() {
   $("form#player").submit(function(event) {
@@ -38,7 +38,10 @@ $(document).ready(function() {
     var player2Name = $("input#player2Name").val();
     var Player1 = new Player(player1Name);
     var Player2 = new Player(player2Name);
-    var die1 = new Die("die1", 1);
+
+    var dice = [];
+    var diceValue = [];
+    var diceTotal = 0;
 
     $(".player1").text(Player1.name);
     $(".player2").text(Player2.name);
@@ -47,104 +50,132 @@ $(document).ready(function() {
     $("#player2score").text(Player2.totalScore);
     $("#player2turn").text(Player2.turnScore);
 
+    $("#dice").show();
+    //moved down to when they enter dice number.
     $("#player1roll").show();
     $("#player2roll").show();
     $("#player1hold").show();
     $("#player2hold").show();
 
+    $("#diceButton").click(function(event){
+      event.preventDefault();
+      var diceTotal = parseInt($("#numberOfDice").val());
+      for (i = 0; i < diceTotal; i++) {
+        dice.push(new Die(("die" + i), 1));
+      }
+    });
+
     $("#player1roll").off();
     $("#player1roll").click(function() {
-      die1.roll();
+      dice.forEach(function(die) {
+        die.roll();
+        diceValue.push(die.value);
+      });
 
-      if (die1.value === 1) {
+      if (diceValue.indexOf(1) != -1 ) {
         alert("Sucks to be you. Turn over. You get nothing. Player 2's turn.");
         Player1.turnScore = 0;
         $("#player1turn").text(Player1.turnScore);
+        diceValue = [];
       } else {
-        Player1.addScore(die1);
+        diceValue.forEach(function(val) {
+          diceTotal += val
+        });
+
+        Player1.addScore(diceTotal);
         $("#player1turn").text(Player1.turnScore);
         alert("Roll again.");
-
       }
     });
 
     $("#player2roll").off();
     $("#player2roll").click(function() {
-      die1.roll();
+      dice.forEach(function(die) {
+        die.roll();
+        diceValue.push(die.value);
+      });
+      debugger;
 
-      if (die1.value === 1) {
+      if (diceValue.indexOf(1) != -1 ) {
         alert("Sucks to be you. Turn over. You get nothing. Player 1's turn.");
         Player2.turnScore = 0;
         $("#player2turn").text(Player2.turnScore);
+        diceValue = [];
       } else {
-        Player2.addScore(die1);
+        diceValue.forEach(function(val) {
+          diceTotal += val
+        });
+        debugger;
+        Player2.addScore(diceTotal);
         $("#player2turn").text(Player2.turnScore);
         alert("Roll again.");
-
       }
     });
-
-    $("#player1hold").off();
-    $("#player1hold").click(function() {
-      Player1.hold();
-      if (Player1.totalScore >= 10) {
-        alert(Player1.name + " Has Won!")
-        if (window.confirm("Play again?")) {
-          Player1.turnScore = 0;
-          Player1.totalScore = 0;
-          $("#player1score").text(Player1.totalScore);
-          $("#player1turn").text(Player1.turnScore);
-          Player2.turnScore = 0;
-          Player2.totalScore = 0;
-          $("#player2score").text(Player2.totalScore);
-          $("#player2turn").text(Player2.turnScore);
-
-        } else {
-          Player1.turnScore = 0;
-          Player1.totalScore = 0;
-          $("#player1score").text(Player1.totalScore);
-          $("#player1turn").text(Player1.turnScore);
-          $(".player1").text("Player 1");
-          $(".player2").text("Player 2");
-          $("#player1roll").hide();
-          $("#player2roll").hide();
-          $("#player1hold").hide();
-          $("#player2hold").hide();
-        }
-      }
-    });
-
-    $("#player2hold").off();
-    $("#player2hold").click(function() {
-      Player2.hold();
-      if (Player2.totalScore >= 10) {
-        alert(Player2.name + " Has Won!")
-        if (window.confirm("Play again?")) {
-          Player2.turnScore = 0;
-          Player2.totalScore = 0;
-          $("#player2score").text(Player2.totalScore);
-          $("#player2turn").text(Player2.turnScore);
-          Player1.turnScore = 0;
-          Player1.totalScore = 0;
-          $("#player1score").text(Player1.totalScore);
-          $("#player1turn").text(Player1.turnScore);
-
-        } else {
-          Player2.turnScore = 0;
-          Player2.totalScore = 0;
-          $("#player2score").text(Player2.totalScore);
-          $("#player2turn").text(Player2.turnScore);
-          $(".player2").text("Player 2");
-          $(".player1").text("Player 1");
-          $("#player2roll").hide();
-          $("#player1roll").hide();
-          $("#player2hold").hide();
-          $("#player1hold").hide();
-        }
-      }
-    });
-    $("input#player1Name").val("")
-    $("input#player2Name").val("")
   });
 
+
+
+
+
+  $("#player1hold").off();
+  $("#player1hold").click(function() {
+    Player1.hold();
+    if (Player1.totalScore >= 10) {
+      alert(Player1.name + " Has Won!")
+      if (window.confirm("Play again?")) {
+        Player1.turnScore = 0;
+        Player1.totalScore = 0;
+        $("#player1score").text(Player1.totalScore);
+        $("#player1turn").text(Player1.turnScore);
+        Player2.turnScore = 0;
+        Player2.totalScore = 0;
+        $("#player2score").text(Player2.totalScore);
+        $("#player2turn").text(Player2.turnScore);
+
+      } else {
+        Player1.turnScore = 0;
+        Player1.totalScore = 0;
+        $("#player1score").text(Player1.totalScore);
+        $("#player1turn").text(Player1.turnScore);
+        $(".player1").text("Player 1");
+        $(".player2").text("Player 2");
+        $("#player1roll").hide();
+        $("#player2roll").hide();
+        $("#player1hold").hide();
+        $("#player2hold").hide();
+      }
+    }
+  });
+
+  $("#player2hold").off();
+  $("#player2hold").click(function() {
+    Player2.hold();
+    if (Player2.totalScore >= 100) {
+      alert(Player2.name + " Has Won!");
+      if (window.confirm("Play again?")) {
+        Player2.turnScore = 0;
+        Player2.totalScore = 0;
+        $("#player2score").text(Player2.totalScore);
+        $("#player2turn").text(Player2.turnScore);
+        Player1.turnScore = 0;
+        Player1.totalScore = 0;
+        $("#player1score").text(Player1.totalScore);
+        $("#player1turn").text(Player1.turnScore);
+
+      } else {
+        Player2.turnScore = 0;
+        Player2.totalScore = 0;
+        $("#player2score").text(Player2.totalScore);
+        $("#player2turn").text(Player2.turnScore);
+        $(".player2").text("Player 2");
+        $(".player1").text("Player 1");
+        $("#player2roll").hide();
+        $("#player1roll").hide();
+        $("#player2hold").hide();
+        $("#player1hold").hide();
+      }
+    }
+  });
+  $("input#player1Name").val("")
+  $("input#player2Name").val("")
 });
