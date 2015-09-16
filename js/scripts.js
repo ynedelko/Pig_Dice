43fc0d5d91862c586063fda7584b0049b3a5939c
Alyssa Horrocks & Bunker <student@epicodus.com>
2015-09-16 12:26:25 -0700

    refactored roll die button

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ function Player(name, turnScore, totalScore) {
   this.name = name;
   this.turnScore = turnScore || 0;
   this.totalScore = totalScore || 0;
-};
+}
 
 Player.prototype.addScore = function (die1) {
   this.turnScore = die1.value + this.turnScore;
@@ -17,10 +17,11 @@ Player.prototype.hold = function() {
 function Die(name, value) {
   this.name = name;
   this.value = value;
-};
+}
 
 Die.prototype.roll = function() {
-  return this.value = Math.floor(Math.random() * (7 - 1)) + 1;
+  this.value = Math.floor(Math.random() * (7 - 1)) + 1;
+  return this.value;
 };
 
 function Computer(name, turnScore, totalScore) {
@@ -35,15 +36,35 @@ $(document).ready(function() {
 
     var player1Name = $("input#player1Name").val();
     var player2Name = $("input#player2Name").val();
-    var newPlayer1 = new Player(player1Name, 0, 0);
-    var newPlayer2 = new Player(player2Name, 0, 0);
+    var Player1 = new Player(player1Name);
+    var Player2 = new Player(player2Name);
+    var die1 = new Die("die1", 1);
 
-    $(".player1").text(newPlayer1.name);
-    $(".player2").text(newPlayer2.name);
-    $("#player1score").text(newPlayer1.totalScore);
-    $("#player1turn").text(newPlayer1.turnScore);
-    $("#player2score").text(newPlayer2.totalScore);
-    $("#player2turn").text(newPlayer2.turnScore);
+    $(".player1").text(Player1.name);
+    $(".player2").text(Player2.name);
+    $("#player1score").text(Player1.totalScore);
+    $("#player1turn").text(Player1.turnScore);
+    $("#player2score").text(Player2.totalScore);
+    $("#player2turn").text(Player2.turnScore);
+
+    $("#player1roll").show();
+    $("#player2roll").show();
+
+    $("#player1roll").off();
+    $("#player1roll").click(function() {
+      die1.roll();
+
+      if (die1.value === 1) {
+        alert("Sucks to be you. Turn over. You get nothing. Player 2's turn.");
+        Player1.turnScore = 0;
+        $("#player1turn").text(Player1.turnScore);
+      } else {
+        Player1.addScore(die1);
+        $("#player1turn").text(Player1.turnScore);
+        alert("Roll again.");
+
+      }
+    });
 
   });
 
